f620e4f5bb74b1be4941a29aebcede2ac3413838
Ossi Berg <78415225+ossiberg@users.noreply.github.com>
2023-08-07 14:24:33 +0300

    fixes a bug in trip duration

--- gtfs2jore3.py
+++ gtfs2jore3.py
@@ -5,10 +5,11 @@ import datetime
 import os
 import writetrip
 import preprocess
+import requests
 
 #writes the output file
 
-def writecsv(datetype, pvm, humanreadableday):
+def writecsv(datetype, pvm, humanreadableday, nextdate):
  date = datetime.date.today()
  date = date.strftime('%Y%m%d')
  time = datetime.datetime.now(tz=None)
@@ -18,11 +19,10 @@ def writecsv(datetype, pvm, humanreadableday):
    jorewriter.writerow([1,'HASTUS','HSL','1.04',date,time])
    jorewriter.writerow([2,configuration['booking'],configuration['kausi'],configuration['kaavio'],datetype,configuration['alkupvm'],configuration['loppupvm'],configuration['liikennoitsija']])
    jorewriter.writerow([3,configuration['kaavio'],datetype,'00','HSL',datetype,configuration['alkupvm'],configuration['loppupvm'],time])
-   writetrip.alltripsgtfstohastus(configuration, humanreadableday, pvm)
-   with open('tmp/trips-' + humanreadableday + '.txt', 'r', encoding='latin-1') as trips:
-       trips = csv.reader(trips, delimiter=';')
-       for a in trips:
-           jorewriter.writerow(a)
+#Rows 4-6, which include separate trips
+   stoptimesandtripsforjore = writetrip.alltripsgtfstohastus(configuration, humanreadableday, nextdate, pvm)
+   for trips in stoptimesandtripsforjore:
+    jorewriter.writerow(trips)
 
 if __name__ == "__main__":
 #get configuration and preprocess gtfs data
@@ -37,8 +37,13 @@ if __name__ == "__main__":
 #loops through dates from which the output file should be generated
  for i in dates:
   humanreadableday = configuration['tarkastelupaivat'][i]
+# provides the next date in order to handle Jore's 30h clock
+  nextdate = datetime.datetime.strptime(humanreadableday, "%Y%m%d")
+  nextdate += datetime.timedelta(days=1)
+  nextdate = str(nextdate)[:10]
+  nextdate = nextdate.replace('-', '')
   try:
    datetype = hastuspar[i]
   except:
    datetype = i
-  writecsv(datetype, i, humanreadableday)
+  writecsv(datetype, i, humanreadableday, nextdate)
